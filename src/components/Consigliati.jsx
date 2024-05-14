916a2b2c5eb31bcc20350485003f79aef09c719a
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-05-14 12:01:16 +0200

    add alertConsigliati e shadow ai button e competenze  da finire

--- src/components/Consigliati.jsx
+++ src/components/Consigliati.jsx
@@ -1,11 +1,13 @@
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import Barra from "./BarraRiempimento";
+import AlertConsigliati from "./Alert";
 
 const Consigliati = () => {
   return (
     <>
       <Card className="p-4 mt-2">
-        <h3>Consigliati per te</h3>
+        <h3 className="fs-5">Consigliati per te</h3>
+
         <span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -19,12 +21,22 @@ const Consigliati = () => {
           >
             <path d="M8 3a8.59 8.59 0 00-8 5 8.54 8.54 0 008 5 8.55 8.55 0 008-5 8.55 8.55 0 00-8-5zm0 8a3 3 0 113-3 3 3 0 01-3 3zm2-3a2 2 0 11-2-2 2 2 0 012 2z"></path>
           </svg>
-          <span className="mx-2">Solo per te</span>
+          <span className="mx-2 textP">Solo per te</span>
         </span>
-        <h5>intermedio</h5> <Barra />
-        <p className="mt-2">
+        <h5 className="textP mb-0 mt-3">intermedio</h5>
+        <Row className="align-items-center">
+          <Col xs={11}>
+            <Barra />
+          </Col>
+          <Col xs={1}>
+            <span>6/7</span>
+          </Col>
+        </Row>
+        <p className=" textP mt-0">
           Completa 1 passaggio per raggiungere il livello Massimo
         </p>
+
+        <AlertConsigliati />
       </Card>
     </>
   );
