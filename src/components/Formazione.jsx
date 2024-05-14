e6029bf5d53da08f200125248e7a0c43d7b82a4e
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-05-14 09:17:23 +0200

    add  pen formazione

--- src/components/Formazione.jsx
+++ src/components/Formazione.jsx
@@ -4,9 +4,12 @@ const Formazione = () => {
   return (
     <>
       <Card className="mt-2 p-4">
-        <div className="d-flex  pencil1 rounded-circle p-1 mt-2  ">
+        <div className="d-flex   p-1 mt-2  ">
           <h3 className="me-auto">Formazione</h3>
-          <div className="pencil1 me-5">
+          <div
+            className="pencil1 rounded-circle d-flex align-items-center justify-content-center me-5 "
+            style={{ width: "50px", height: "50px" }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               id="Outline"
@@ -17,24 +20,31 @@ const Formazione = () => {
               <path d="M23,11H13V1a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V11H1a1,1,0,0,0-1,1H0a1,1,0,0,0,1,1H11V23a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13H23a1,1,0,0,0,1-1h0A1,1,0,0,0,23,11Z" />
             </svg>
           </div>
-          <Image src={pencil} width={30} height={30} />
+          <div
+            className="pencil1 rounded-circle d-flex align-items-center justify-content-center "
+            style={{ width: "50px", height: "50px" }}
+          >
+            <Image src={pencil} width={30} height={30} />
+          </div>
         </div>
         <Row>
           <Col xs={3}>
             <Image
               src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/EPICODE_resized/original.png?1651791536"
               roundedCircle
-              width={70}
-              height={70}
+              width={50}
+              height={50}
             />
           </Col>
           <Col xs={9}>
-            <h3>EPICODE</h3>
+            <h4>EPICODE</h4>
             <p>
               sviluppatore front end e back end, full stack
               developersviluppatore front end e back end, full stack developer
-              feb 2024 - ago 2024feb 2024 - ago 2024 SASS, JavaScript e +3
-              competenze
+              feb 2024 - ago 2024feb 2024 - ago 2024
+              <p style={{ fontWeight: "bold" }}>
+                SASS, JavaScript e +3 competenze
+              </p>
             </p>
           </Col>
         </Row>
@@ -43,19 +53,19 @@ const Formazione = () => {
             <Image
               src="https://iio.azcast.arizona.edu/sites/default/files/profile-blank-whitebg.png"
               roundedCircle
-              width={70}
-              height={70}
+              width={50}
+              height={50}
             />
           </Col>
           <Col xs={9}>
-            <h3>ITIS FERMI</h3>
+            <h4>ITIS FERMI</h4>
             <p>
               Diploma di Specializzazione di 1° livello DS1 in scienze e
               tecnologie informatiche, Tecnologia/tecnico di ingegneria dell
               automazione Diploma di Specializzazione di 1° livello DS1 in
               scienze e tecnologie informatiche, Tecnologia/tecnico di
               ingegneria dell automazione set 2014 - lug 2018set 2014 - lug 2018
-              Lingua inglese e C++
+              <p style={{ fontWeight: "bold" }}>Lingua inglese e C++</p>
             </p>
           </Col>
         </Row>
