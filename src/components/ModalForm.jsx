bd56945ae669457281b3c96a30fa99f14264f864
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-14 19:30:27 +0200

    modal

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -4,12 +4,12 @@ import {
   Col,
   Collapse,
   Form,
+  ListGroup,
   Modal,
   Row,
 } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { PostExperienceAction } from "../redux/actions/Experience";
-import { Link } from "react-router-dom";
 
 const ModalForm = (props) => {
   const information = useSelector(
@@ -26,6 +26,8 @@ const ModalForm = (props) => {
     );
   };
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
   const [informationPut, setinformationPut] = useState({
     role: "",
     companyName: "",
@@ -57,31 +59,199 @@ const ModalForm = (props) => {
               aria-expanded={open}
               size="lg"
               variant="light"
-              className="bg-white border-0"
+              className="bg-white border-0 d-flex align-items-center justify-content-between"
             >
-              click
+              <p className="mb-0 fw-bold">
+                Sezioni principali
+              </p>
+              <p className="mb-0 fw-bold">
+                {open ? (
+                  <i className="bi bi-caret-up"></i>
+                ) : (
+                  <i className="bi bi-caret-down"></i>
+                )}{" "}
+              </p>
             </Button>
           </div>
 
           <Collapse in={open}>
             <div id="example-collapse-text">
-              Anim pariatur cliche reprehenderit, enim
-              eiusmod high life accusamus terry richardson
-              ad squid. Nihil anim keffiyeh helvetica, craft
-              beer labore wes anderson cred nesciunt
-              sapiente ea proident.
+              <ListGroup>
+                <ListGroup.Item>
+                  <p className="fs-8 text-secondary mb-0 ">
+                    Iniziamo dalle basi. Se compili queste
+                    sezioni, sarà più facile trovarti per i
+                    recluter e per le persone che potresti
+                    conoscere
+                  </p>
+                </ListGroup.Item>
+
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi grado di formazione
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p
+                    className="fs-8 text-secondary mb-0 fw-bold"
+                    onClick={() => {
+                      props.setShow(false);
+                      setShow1(true);
+                    }}
+                  >
+                    {" "}
+                    Aggiungi posizione lavorativa
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi pausa lavorativa
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi competenze
+                  </p>
+                </ListGroup.Item>
+              </ListGroup>
             </div>
           </Collapse>
-          <Link
-            to={"/"}
-            onClick={() => {
-              props.setShow(false);
-              setShow1(true);
-            }}
-            className="fs-10"
-          >
-            Aggiungi posizione lavorativa
-          </Link>
+          <div className="d-grid gap-2">
+            <Button
+              onClick={() => setOpen2(!open2)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open2}
+              size="lg"
+              variant="light"
+              className="bg-white border-0 d-flex align-items-center justify-content-between"
+            >
+              <p className="mb-0 fw-bold">
+                Sezioni consigliate
+              </p>
+              <p className="mb-0 fw-bold">
+                {open2 ? (
+                  <i className="bi bi-caret-up"></i>
+                ) : (
+                  <i className="bi bi-caret-down"></i>
+                )}{" "}
+              </p>
+            </Button>
+          </div>
+
+          <Collapse in={open2}>
+            <div id="example-collapse-text">
+              <ListGroup>
+                <ListGroup.Item>
+                  <p className="fs-8 text-secondary mb-0 ">
+                    Completando queste sezioni aumenterai la
+                    tua credibilità e potrei accedere a più
+                    opportunità
+                  </p>
+                </ListGroup.Item>
+
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi elementi in primo piano
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi licenze e certificazioni
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi progetti
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi corsi
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi referenze
+                  </p>
+                </ListGroup.Item>
+              </ListGroup>
+            </div>
+          </Collapse>
+          <div className="d-grid gap-2">
+            <Button
+              onClick={() => setOpen3(!open3)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open3}
+              size="lg"
+              variant="light"
+              className="bg-white border-0 d-flex align-items-center justify-content-between"
+            >
+              <p className="mb-0 fw-bold">Altro</p>
+              <p className="mb-0 fw-bold">
+                {open3 ? (
+                  <i className="bi bi-caret-up"></i>
+                ) : (
+                  <i className="bi bi-caret-down"></i>
+                )}{" "}
+              </p>
+            </Button>
+          </div>
+
+          <Collapse in={open3}>
+            <div id="example-collapse-text">
+              <ListGroup>
+                <ListGroup.Item>
+                  <p className="fs-8 text-secondary mb-0 ">
+                    Conferisci ancora più personalità al tuo
+                    profilo. Queste sezioni ti aiuteranno a
+                    espandere la tua rete e istaurare più
+                    relazioni lavorative.
+                  </p>
+                </ListGroup.Item>
+
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi esperienza di volontariato
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi pubblicazioni
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi brevetti
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi riconoscimenti e premi
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi votazioni esame
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi lingue
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi organizzazioni
+                  </p>
+                </ListGroup.Item>
+                <ListGroup.Item className="border-bottom">
+                  <p className="fs-8 text-secondary mb-0 fw-bold">
+                    Aggiungi cause
+                  </p>
+                </ListGroup.Item>
+              </ListGroup>
+            </div>
+          </Collapse>
         </Modal.Body>
       </Modal>
       <Modal
