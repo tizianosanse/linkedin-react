6180f4d3b817f6cc4b99da417d1c4310f1241aa2
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-14 18:02:11 +0200

    refactor

--- src/components/ProfileInformation.jsx
+++ src/components/ProfileInformation.jsx
@@ -1,52 +1,29 @@
 import { useEffect, useState } from "react";
-import {
-  Col,
-  Collapse,
-  Form,
-  Image,
-  Modal,
-  Row,
-} from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import { Button } from "react-bootstrap/esm";
 import { useDispatch, useSelector } from "react-redux";
 import { getInformation } from "../redux/actions/ProfileInformationActions";
 import { Link } from "react-router-dom";
 import pencil from "../assets/icons8-pencil-48.png";
-import { PostExperienceAction } from "../redux/actions/Experience";
+
+import ModalForm from "./ModalForm";
 
 const ProfileInformation = () => {
   const [show, setShow] = useState(false);
-  const [show1, setShow1] = useState(false);
-  const [open, setOpen] = useState(false);
+
   const information = useSelector(
     (state) => state.ProfileInformation.content
   );
-  const id = information._id;
-  const [informationPut, setinformationPut] = useState({
-    role: "",
-    companyName: "",
-    area: "",
-    monthStart: "Mese",
-    monthEnd: "Mese",
-    yearStart: "Anno",
-    yearEnd: "Anno",
-    description: "",
-  });
-  console.log(id);
+
   const handleClose = () => setShow(false);
-  const handleClose1 = () => setShow1(false);
+
   const handleShow = () => setShow(true);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch(
-      PostExperienceAction(id, "POST", informationPut)
-    );
-  };
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getInformation());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -132,289 +109,11 @@ const ProfileInformation = () => {
             Mostra dettagli
           </Link>
         </div>
-        <Modal
+        <ModalForm
           show={show}
-          onHide={handleClose}
-          animation={false}
-          size="lg"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title className="fs-5">
-              Aggiungi al profilo
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="d-grid gap-2">
-              <Button
-                onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open}
-                size="lg"
-                variant="light"
-                className="bg-white border-0"
-              >
-                click
-              </Button>
-            </div>
-
-            <Collapse in={open}>
-              <div id="example-collapse-text">
-                Anim pariatur cliche reprehenderit, enim
-                eiusmod high life accusamus terry richardson
-                ad squid. Nihil anim keffiyeh helvetica,
-                craft beer labore wes anderson cred nesciunt
-                sapiente ea proident.
-              </div>
-            </Collapse>
-            <Link
-              to={"/"}
-              onClick={() => {
-                setShow(false);
-                setShow1(true);
-              }}
-              className="fs-10"
-            >
-              Aggiungi posizione lavorativa
-            </Link>
-          </Modal.Body>
-        </Modal>
-        <Modal
-          show={show1}
-          onHide={handleClose1}
-          animation={false}
-          size="lg"
-        >
-          <Modal.Header className="px-4" closeButton>
-            <Modal.Title className="fs-5">
-              Aggiungi esperienza
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body className="px-4">
-            <Form onSubmit={handleSubmit}>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Qualifica*
-                </Form.Label>
-                <Form.Control
-                  size="sm"
-                  type="text"
-                  value={informationPut.title}
-                  onChange={(e) => {
-                    setinformationPut({
-                      ...informationPut,
-                      role: e.target.value,
-                    });
-                  }}
-                  placeholder="Esempio: Retail Sales Manager"
-                  className="border-black "
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Nome Azienda*
-                </Form.Label>
-                <Form.Control
-                  size="sm"
-                  type="text"
-                  value={informationPut.companyName}
-                  onChange={(e) => {
-                    setinformationPut({
-                      ...informationPut,
-                      companyName: e.target.value,
-                    });
-                  }}
-                  placeholder="Esempio: Microsoft"
-                  className="border-black "
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Località*
-                </Form.Label>
-                <Form.Control
-                  size="sm"
-                  type="text"
-                  value={informationPut.area}
-                  onChange={(e) => {
-                    setinformationPut({
-                      ...informationPut,
-                      area: e.target.value,
-                    });
-                  }}
-                  placeholder="Esempio: Milano, Italia"
-                  className="border-black "
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Descrizione*
-                </Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  value={informationPut.description}
-                  onChange={(e) => {
-                    setinformationPut({
-                      ...informationPut,
-                      description: e.target.value,
-                    });
-                  }}
-                  placeholder=""
-                  className="border-black "
-                />
-              </Form.Group>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Data di inizio*
-                </Form.Label>
-                <Row>
-                  <Col xs={6}>
-                    <Form.Select
-                      aria-label="Default select example"
-                      onChange={(e) => {
-                        setinformationPut({
-                          ...informationPut,
-                          monthStart: e.target.value,
-                        });
-                      }}
-                    >
-                      <option>
-                        {informationPut.monthStart}
-                      </option>
-                      <option value="1">Gennaio</option>
-                      <option value="2">Febbraio</option>
-                      <option value="3">Marzo</option>
-                      <option value="4">Aprile</option>
-                      <option value="5">Maggio</option>
-                      <option value="6">Giugno</option>
-                      <option value="7">Luglio</option>
-                      <option value="8">Agosto</option>
-                      <option value="9">Settembre</option>
-                      <option value="10">Ottobre</option>
-                      <option value="11">Novembre</option>
-                      <option value="12">Dicembre</option>
-                    </Form.Select>
-                  </Col>
-                  <Col xs={6}>
-                    <Form.Select
-                      aria-label="Default select example"
-                      onChange={(e) => {
-                        setinformationPut({
-                          ...informationPut,
-                          yearStart: e.target.value,
-                        });
-                      }}
-                    >
-                      <option>
-                        {informationPut.yearStart}
-                      </option>
-                      <option value="2024">2024</option>
-                      <option value="2023">2023</option>
-                      <option value="2022">2022</option>
-                      <option value="2021">2021</option>
-                      <option value="2020">2020</option>
-                      <option value="2019">2019</option>
-                      <option value="2018">2018</option>
-                      <option value="2017">2017</option>
-                      <option value="2016">2016</option>
-                      <option value="2015">2015</option>
-                      <option value="2014">2014</option>
-                      <option value="2013">2013</option>
-                    </Form.Select>
-                  </Col>
-                </Row>
-              </Form.Group>
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label className="fw-light mb-0">
-                  Data di fine*
-                </Form.Label>
-                <Row>
-                  <Col xs={6}>
-                    <Form.Select
-                      aria-label="Default select example"
-                      onChange={(e) => {
-                        setinformationPut({
-                          ...informationPut,
-                          monthEnd: e.target.value,
-                        });
-                      }}
-                    >
-                      <option>
-                        {informationPut.monthEnd}
-                      </option>
-                      <option value="1">Gennaio</option>
-                      <option value="2">Febbraio</option>
-                      <option value="3">Marzo</option>
-                      <option value="4">Aprile</option>
-                      <option value="5">Maggio</option>
-                      <option value="6">Giugno</option>
-                      <option value="7">Luglio</option>
-                      <option value="8">Agosto</option>
-                      <option value="9">Settembre</option>
-                      <option value="10">Ottobre</option>
-                      <option value="11">Novembre</option>
-                      <option value="12">Dicembre</option>
-                    </Form.Select>
-                  </Col>
-                  <Col xs={6}>
-                    <Form.Select
-                      aria-label="Default select example"
-                      onChange={(e) => {
-                        setinformationPut({
-                          ...informationPut,
-                          yearEnd: e.target.value,
-                        });
-                      }}
-                    >
-                      <option>
-                        {informationPut.yearEnd}
-                      </option>
-                      <option value="2024">2024</option>
-                      <option value="2023">2023</option>
-                      <option value="2022">2022</option>
-                      <option value="2021">2021</option>
-                      <option value="2020">2020</option>
-                      <option value="2019">2019</option>
-                      <option value="2018">2018</option>
-                      <option value="2017">2017</option>
-                      <option value="2016">2016</option>
-                      <option value="2015">2015</option>
-                      <option value="2014">2014</option>
-                      <option value="2013">2013</option>
-                    </Form.Select>
-                  </Col>
-                </Row>
-              </Form.Group>
-
-              <div className="border-top p-3 d-flex">
-                <Button
-                  className="rounded-pill px-3 modal-btn ms-auto"
-                  type="submit"
-                >
-                  Salva
-                </Button>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
+          handleClose={handleClose}
+          setShow={setShow}
+        />
       </div>
     </>
   );
