f3888bdd4c14f9946e884fa3af7cf4b277e24aca
Emanuele Umberto Mangano <emanuelemangano15@gmail.com>
2024-05-16 17:22:25 +0200

    fix-image-jobsPremium

--- src/components/Jobs/JobsPremium.jsx
+++ src/components/Jobs/JobsPremium.jsx
@@ -11,9 +11,7 @@ const JobsPremium = () => {
   const dispatch = useDispatch();
   const premium = useSelector((state) => state.Jobs.premiumJobs);
   console.log(premium);
-  const information = useSelector(
-    (state) => state.ProfileInformation.informationJobPrem
-  );
+  const information = useSelector((state) => state.ProfileInformation.informationJobPrem);
   console.log(premium);
   useEffect(() => {
     dispatch(getJobsPremium());
@@ -27,9 +25,7 @@ const JobsPremium = () => {
       {" "}
       <div className=" bg-white p-3 rounded-top-3 border border-1 exploreOpportunity mt-3">
         <h2 className="mb-0 mt-2">Offerte Premium</h2>
-        <p className="mb-0">
-          Sulla base del tuo profilo e della tua cronologia delle ricerche
-        </p>
+        <p className="mb-0">Sulla base del tuo profilo e della tua cronologia delle ricerche</p>
         <Row className="align-items-center">
           {" "}
           <Col xs={2}>
@@ -41,19 +37,11 @@ const JobsPremium = () => {
             />
           </Col>
           <Col xs={10}>
-            <h4 className="fs-6">
-              Vedi l elenco delle offerte di lavoro alle quali ti sei candidato
-            </h4>
-            <Button className="btn-warning btn-premium">
-              Prova Premium per 0 euro
-            </Button>
+            <h4 className="fs-6">Vedi l elenco delle offerte di lavoro alle quali ti sei candidato</h4>
+            <Button className="btn-warning btn-premium">Prova Premium per 0 euro</Button>
           </Col>
         </Row>
 
-        <p className="mb-0">
-          Sulla base del tuo profilo e della tua cronologia delle ricerche
-        </p>
-        <Image src={information.image} width={50} height={50} />
         {premium.map((prem) => {
           console.log(prem);
           return (
@@ -71,13 +59,7 @@ const JobsPremium = () => {
         <Link className="text-decoration-none p-2 text-black " to="">
           Mostra tutto
           <span className="ms-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-              viewBox="0 -6.5 38 38"
-              version="1.1"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 -6.5 38 38" version="1.1">
               <g id="icons" stroke="none" fill="none">
                 <g
                   id="ui-gambling-website-lined-icnos-casinoshunter"
